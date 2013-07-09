9bfe655e7efea7620db02784162524009a51a059
Heiko Adams <heiko.adams@gmail.com>
2013-07-09 20:52:00 +0200

    Bugfixes and improvements

--- generate.sh
+++ generate.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 clear
-CURRENT=`pwd`
+CURRENTPATH=`realpath $0`
+CURRENT=`dirname $CURRENTPATH`
 echo "Generating exceptionrules.txt ..."
 echo -e "[Adblock Plus 2.0]\n! Expires: 1 days" > $CURRENT/exceptionrules.txt
 cat $CURRENT/data/misc.dat >> $CURRENT/exceptionrules.txt
@@ -8,7 +9,7 @@ cat $CURRENT/data/blogs.dat >> $CURRENT/exceptionrules.txt
 cat $CURRENT/data/news.dat >> $CURRENT/exceptionrules.txt
 echo "Generating completed!"
 
-if ![ -e $CURRENT/upload.sh ]
+if [ ! -e $CURRENT/upload.sh ]
 then
 	echo -e "\nNo upload.sh found. Uploading to ftp canceled!"
 	echo -e "You can use the upload.sh.dist file as a template"
