6ab0e4d159f9681defd9a46242d27a922ec870f4
Heiko Adams <heiko.adams@gmail.com>
2013-07-10 09:31:17 +0200

    Check if data files are modified before generating exceptionlist

--- generate.sh
+++ generate.sh
@@ -2,19 +2,27 @@
 clear
 CURRENTPATH=`realpath $0`
 CURRENT=`dirname $CURRENTPATH`
-echo "Generating exceptionrules.txt ..."
-echo -e "[Adblock Plus 2.0]\n! Expires: 1 days" > $CURRENT/exceptionrules.txt
-cat $CURRENT/data/misc.dat >> $CURRENT/exceptionrules.txt
-cat $CURRENT/data/blogs.dat >> $CURRENT/exceptionrules.txt
-cat $CURRENT/data/news.dat >> $CURRENT/exceptionrules.txt
-echo "Generating completed!"
 
-if [ ! -e $CURRENT/upload.sh ]
+if [ -N $CURRENT/data/misc.dat ] || [ -N $CURRENT/data/blogs.dat ] || [ -N $CURRENT/data/news.dat ]
 then
-	echo -e "\nNo upload.sh found. Uploading to ftp canceled!"
-	echo -e "You can use the upload.sh.dist file as a template"
-	echo -e "for creating your own upload.sh upload script."
+	echo "Generating exceptionrules.txt ..."
+	echo -e "[Adblock Plus 2.0]\n! Expires: 1 days" > $CURRENT/exceptionrules.txt
+	cat $CURRENT/data/misc.dat >> $CURRENT/exceptionrules.txt
+	echo -e "\n" >> $CURRENT/exceptionrules.txt
+	cat $CURRENT/data/blogs.dat >> $CURRENT/exceptionrules.txt
+	echo -e "\n" >> $CURRENT/exceptionrules.txt
+	cat $CURRENT/data/news.dat >> $CURRENT/exceptionrules.txt
+	echo "Generating completed!"
+
+	if [ ! -e $CURRENT/upload.sh ]
+	then
+		echo -e "\nNo upload.sh found. Uploading to ftp canceled!"
+		echo -e "You can use the upload.sh.dist file as a template"
+		echo -e "for creating your own upload.sh upload script."
+	else
+		echo -e "\nUploading exceptionrules.txt to remote host"
+		$CURRENT/upload.sh
+	fi
 else
-	echo -e "\nUploading exceptionrules.txt to remote host"
-	$CURRENT/upload.sh
+	echo "Nothing to do!"
 fi
