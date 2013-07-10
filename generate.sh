fb2128fc636cb7e4b9adc5885afe542f9fbbf7df
Heiko Adams <heiko.adams@gmail.com>
2013-07-10 09:37:42 +0200

    Write timestamp of last update to exceptionrules.txt

--- generate.sh
+++ generate.sh
@@ -2,16 +2,21 @@
 clear
 CURRENTPATH=`realpath $0`
 CURRENT=`dirname $CURRENTPATH`
+TIMESTAMP=$(date)
+
+echo $TIMESTAMP
 
 if [ -N $CURRENT/data/misc.dat ] || [ -N $CURRENT/data/blogs.dat ] || [ -N $CURRENT/data/news.dat ]
 then
 	echo "Generating exceptionrules.txt ..."
 	echo -e "[Adblock Plus 2.0]\n! Expires: 1 days" > $CURRENT/exceptionrules.txt
+	echo -e "! Last update: $TIMESTAMP" >> $CURRENT/exceptionrules.txt
 	cat $CURRENT/data/misc.dat >> $CURRENT/exceptionrules.txt
 	echo -e "\n" >> $CURRENT/exceptionrules.txt
 	cat $CURRENT/data/blogs.dat >> $CURRENT/exceptionrules.txt
 	echo -e "\n" >> $CURRENT/exceptionrules.txt
 	cat $CURRENT/data/news.dat >> $CURRENT/exceptionrules.txt
+	echo -e "\n" >> $CURRENT/exceptionrules.txt
 	echo "Generating completed!"
 
 	if [ ! -e $CURRENT/upload.sh ]
